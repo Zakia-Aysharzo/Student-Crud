8dd02e67d5a343ed77a07fbe05c20083ee3e1ce3
Zakia_Aysharzo <zakia47cse@gmail.com>
2021-08-16 21:51:42 +0600

    My Project

---
+++ resources/views/students/edit.blade.php
@@ -0,0 +1,54 @@
+@extends('students.layout')
+
+@section('content')
+
+<div class="row">
+	<div class="col-lg-12 margin-tb">
+		<div class="pull-left">
+			<h2>Edited</h2>
+		</div>
+		<div class="pull-right">
+			<a class="btn btn-primary" href="{{route('students.index')}}">Back</a>
+		</div>
+	</div>
+</div>	
+@if($errors->any())
+<div class="alert alert-danger">
+	<strong>Oooops</strong>There were some problems<br><br>
+	<ul>
+		
+		@foreach($errors->all() as $error)
+		<li>{{$error}}</li>
+		@endforeach
+	</ul>
+</div>
+@endif
+<form action="{{route('students.update',$student->id)}}" method="POST">
+	@csrf
+	@method('PUT')
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<div class="form-group">
+				<strong>StudentName</strong>
+				<input type="text" name="stdName"value="{{$student->stdName}}" class="form-control" placeholder="Name">
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<div class="form-group">
+				<strong>Course:</strong>
+				<input type="text" name="course"value="{{$student->course}}" class="form-control" placeholder="Course">
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<div class="form-group">
+				<strong>Fee</strong>
+				<input type="number" name="Fee"value="{{$student->Fee}}" class="form-control" placeholder="Fee">
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12 text-center">
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</div>
+
+	</div>
+</form>
+@endsection
